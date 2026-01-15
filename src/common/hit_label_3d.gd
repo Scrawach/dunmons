564d115e7df4037a6c68662c3d0ff956a33fd100
scrawach <scrawach@gmail.com>
2026-01-16 00:48:48 +0300

    fix: spread for hit label 3d

--- src/common/hit_label_3d.gd
+++ src/common/hit_label_3d.gd
@@ -3,7 +3,7 @@ extends Label3D
 
 func launch(new_text: String) -> void:
 	text = new_text
-	var random_direction := get_random_direction()
+	var random_direction := get_random_direction() / 2
 	var tween = create_tween()
 	tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SPRING)
 	tween.tween_property(self, "position", position + random_direction, 0.4)
@@ -16,4 +16,4 @@ func launch(new_text: String) -> void:
 func get_random_direction() -> Vector3:
 	var x := randf_range(-1, 1)
 	var z := randf_range(-1, 1)
-	return Vector3(x, 2, z)
+	return Vector3(x, 3, z)
