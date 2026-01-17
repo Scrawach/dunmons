22d54a43a311f9297ffbf6b03b6febb40f2d9509
scrawach <scrawach@gmail.com>
2026-01-17 11:39:12 +0300

    feat: paint start battle button, a bit timing fixes for target circle

--- src/gameloop/battle/target_circle.gd
+++ src/gameloop/battle/target_circle.gd
@@ -12,7 +12,7 @@ func show_under(target: Node3D) -> void:
 	if not is_visible_in_tree():
 		show()
 		global_position = target.global_position
-		tween.tween_property(self, "scale", Vector3.ONE, 0.4).from(Vector3.ZERO)
+		tween.tween_property(self, "scale", Vector3.ONE, 0.6).from(Vector3.ZERO)
 	else:
 		tween.tween_property(self, "global_position", target.global_position, 0.4)
 
@@ -20,7 +20,7 @@ func smooth_hide() -> void:
 	_stop_if_needed()
 	tween = create_tween()
 	tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SPRING)
-	tween.tween_property(self, "scale", Vector3.ZERO, 0.5)
+	tween.tween_property(self, "scale", Vector3.ZERO, 0.8)
 	tween.tween_callback(hide)
 
 func _stop_if_needed() -> void:
